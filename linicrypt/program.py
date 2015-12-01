42fd414c7a729d1478586292ba46a3b9ad7d7676
Brent Carmer <carmerb@eecs.oregonstate.edu>
2015-12-01 14:29:27 -0800

    DSL for writing linicrypt programs

---
+++ linicrypt/program.py
@@ -0,0 +1,84 @@
+# dsl for building linicrypt programs
+from sage.all import *
+from linicrypt.types import *
+
+class constraint:
+    def __init__(self, t, Q, a):
+        self.t = t
+        self.Q = Q
+        self.a = a
+    def __repr__(self):
+        return "<{0.t}, {0.Q}, {0.a}>".format(self)
+    def lhs(self): return self.Q
+    def rhs(self): return self.a
+
+class env (dict):
+    def __init__(self, field): 
+        self._next_ref = 0
+        self._next_rand_id = 0
+        self.output = []
+        self.field = field
+
+    def next_rand_id(self):
+        tmp = self._next_rand_id
+        self._next_rand_id += 1
+        return tmp
+
+    def next_ref(self):
+        tmp = self._next_ref
+        self._next_ref += 1
+        return tmp
+
+    def lookup(self, ref): 
+        return self[ref]    
+
+    def insert(self, elem):
+        inv = {v:k for k,v in self.iteritems()}
+        if elem in inv: return inv[elem]
+        r = self.next_ref()
+        self[r] = elem
+        return r
+
+    def to_linicrypt(self):
+        n = len(filter(lambda e: e[0] in ["Rand", "H"], self.itervalues()))
+        fresh_ctr = 0
+        t_ctr = 0
+        mapping = {}
+        A = []
+        C = []
+        for ref, expr in self.iteritems():
+            if expr[0] == "Rand":
+                row = vector(self.field, [0]*n)
+                row[fresh_ctr] = 1
+                fresh_ctr += 1
+            elif expr[0] == "H":
+                row = vector(self.field, [0]*n)
+                row[fresh_ctr] = 1
+                fresh_ctr += 1
+                Q = [ A[mapping[arg]] for arg in expr[1] ]
+                C.append(constraint(t_ctr, Q, row))
+                t_ctr += 1
+            elif expr[0] == "Plus":
+                row = vector(self.field, [0]*n)
+                for arg in expr[1]:
+                    row += A[mapping[arg]]
+            else:
+                raise Exception("Unknown instruction: " + expr[0])
+            mapping[ref] = len(A)
+            A.append(row)
+        return (matrix(self.field, A), C)
+    
+def Rand(env):
+    ident = env.next_rand_id()
+    return env.insert(("Rand", ident))
+
+def Plus(env, *args):
+    return env.insert(("Plus", args))
+
+def H(env, *args):
+    return env.insert(("H", args))
+
+def Output(env, *args):
+    assert(not len(env.output))
+    env.output = args
+
--- test.py
+++ test.py
@@ -1,10 +1,28 @@
 from sage.all import *
-from linicrypt.types import *
 from linicrypt.normalize import *
+from linicrypt.program import *
 
 f = GF(2)
-def mat(xs): return matrix(f, xs)
-def vec(xs): return vector(f, xs)
 
-test_m = mat([[1,0,0],[0,1,0],[1,1,0],[0,0,1],[1,0,1]])
-test_c = [Constraint(0, mat([[1,1,0]]), vec([0,0,1]))]
+def security_game(Gb, a1, a2, b1, b2):
+    e = env(f)
+    A     = Rand(e)
+    B     = Rand(e)
+    delta = Rand(e)
+    (F1, F2, C) = Gb(e, b1, b2, A, B, delta)
+    Ap = Plus(e, A, delta) if a1 + b1 else A
+    Bp = Plus(e, B, delta) if a2 + b2 else B
+    Cp = Plus(e, C, delta) if 1 + res(a1, a2, b1, b2) else C
+    Output(e, Ap, Bp, F1, F2, H(Cp))
+    return e
+
+def test():
+    e = env(f)
+    v1 = Rand(e)
+    v2 = Rand(e)
+    v3 = Plus(e, v1, v2)
+    v4 = H(e, v3)
+    v5 = Plus(e, v4, v1)
+    Output(e, v3, v5)
+    return e
+
